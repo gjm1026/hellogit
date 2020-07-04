53bb7f19a8d6f053e46362fc38aff2f588ac7bc6
gjm1026 <gjm1026@naver.com>
2020-07-04 17:20:36 +0900

    add log

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
 function add(a,b)
 {
-	return a+b;
+	var ret = a+b;
+	console.log("add result : " + ret)
+	return ret
 }
 
 function minus(a,b)
 {
-	return a-b;
+	var ret = a-b
+	console.log("minus result :" +ret)
+	return ret
 }
